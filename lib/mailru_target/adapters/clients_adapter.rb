ac6bbd1eab0af76507b948b8c96fd5630048ed2d
Gennady Novoselov <kudikina-gora@yandex.ru>
2015-07-20 19:16:12 +0300

    Add entity adapters for urls to be inside gem

--- lib/mailru_target/adapters/clients_adapter.rb
+++ lib/mailru_target/adapters/clients_adapter.rb
@@ -9,5 +9,18 @@ module MailruTarget
       request(:post, "/clients.json", presenter)
     end
 
+    def get_token(presenter)
+      request(
+        :post,
+        "/oauth2/token.json",
+        {
+          grant_type: "agency_client_credentials",
+          client_id: presenter[:client_id],
+          client_secret: presenter[:client_secret],
+          agency_client_name: presenter[:client_username]
+        }
+      )
+    end
+
   end
 end
