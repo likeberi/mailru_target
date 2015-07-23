a0e2168ac02de9bdb293746d378539eaf118f6ef
Gennady Novoselov <kudikina-gora@yandex.ru>
2015-07-23 15:51:50 +0300

    Add entity adapters for urls to be inside gem

--- lib/mailru_target/adapters/images_adapter.rb
+++ lib/mailru_target/adapters/images_adapter.rb
@@ -3,6 +3,7 @@ module MailruTarget
 
     def create_image(presenter)
       presenter[:image_file] = File.new(presenter[:image_file], "rb")
+      # Passing empty grant_type here for params not to be converted into JSON (@see #build method in request.rb)
       request(:post, "/images.json", presenter.merge({ grant_type: '' }))
     end
 
